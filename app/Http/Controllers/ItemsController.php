30124517e1ab24af8b8f1a55dd201f31cecc1966
tatsuya.miyazaki <ec2-user@ip-172-31-3-82.us-east-2.compute.internal>
2018-05-30 10:31:01 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Item;
+
 class ItemsController extends Controller
 {
     public function create()
