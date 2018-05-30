97820b6cb5d1292917057345b7c98fb6ac6f7c7e
tatsuya.miyazaki <ec2-user@ip-172-31-3-82.us-east-2.compute.internal>
2018-05-30 08:56:15 +0000

    want

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        $count_want = $user->want_items()->count();
+        $items = \DB::table('items')->join('item_user', 'items.id', '=', 'item_user.item_id')->select('items.*')->where('item_user.user_id', $user->id)->distinct()->groupBy('items.id')->paginate(20);
+
+        return view('users.show', [
+            'user' => $user,
+            'items' => $items,
+            'count_want' => $count_want,
+            'count_have' => $count_have,
+        ]);
+    }
+}
